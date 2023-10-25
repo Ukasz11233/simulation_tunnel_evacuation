9e6659c2460086a968fef01ad6567cce8be38565
Ukasz11233 <chmiel11233@gmail.com>
2023-10-25 21:07:57 +0200

    Refactored code - simplified and increased efficiency. Also rescaled map - still TODO

--- classes/Board.py
+++ classes/Board.py
@@ -5,6 +5,8 @@ from classes.Cell import *
 class Board:
 
     def __init__(self, _screen) -> None:
+        self.cellSize = WIDTH // BOARD_WIDTH
+
         self.boardWidth = BOARD_WIDTH
         self.boardHeight = BOARD_HEIGHT
         self.screen = _screen
@@ -16,7 +18,14 @@ class Board:
     def drawBoard(self):
         for x in range(self.boardWidth):
             for y in range(self.boardHeight):
-                self.board[x][y].draw()
+                self.drawCell(x, y)
+
+    def drawCell(self, x, y):
+        def _drawCellColored(color):
+            pygame.draw.rect(self.screen, color, (x * self.cellSize, y, self.cellSize, self.cellSize))
+
+        if(self.board[x][y].isObstacle()):
+            _drawCellColored(RED)
 
 
     def createWalls(self):
@@ -24,82 +33,76 @@ class Board:
         self.drawBottomWall()
         self.drawInnerWalls()
 
-
-        #     self.board[x][self.boardHeight - 1].setObstacle(x, self.boardHeight - 1)
-
-        # for y in range(self.boardHeight):
-        #     self.board[0][y].setObstacle(0, y)
-            
-        #     self.board[self.boardWidth - 1][y].setObstacle(self.boardWidth - 1, y)
-
+        
     def drawTopWall(self):
         y = 10
-        for x in range(500):
-            self.board[x][y].setObstacle(x ,y)
-            if(x % 50 == 0):
+        for x in range(250):
+            self.board[x][y].setObstacle()
+            if(x % 25 == 0):
                 y -= 1
 
             if(y < 0):
                 print(y)
-        for x in range(500, self.boardWidth):
-            self.board[x][0].setObstacle(x, 0)
+        for x in range(250, self.boardWidth):
+            self.board[x][0].setObstacle()
 
     def drawBottomWall(self):
         y_bottom_left = self.boardHeight-7
         x_start = 0
-        x_end = 84
+        x_end = 42
         for x in range(x_start, x_end):
-            self.board[x][y_bottom_left].setObstacle(x, y_bottom_left)
-            if( x % 14 == 0):
+            self.board[x][y_bottom_left].setObstacle()
+            if( x % 7 == 0):
                 y_bottom_left += 1
+        
         x_start = x_end
-        x_end = 84 + 576
+        x_end = 42 + 288
         for x in range(x_start, x_end):
-            self.board[x][y_bottom_left].setObstacle(x, y_bottom_left)
-            if(x % 57 == 0):
+            self.board[x][y_bottom_left].setObstacle()
+            if(x % 28 == 0):
                 y_bottom_left -= 1
             
         x_start = x_end
-        x_end = 84 + 576 + 942
+        x_end = 42 + 288 + 471
         for x in range(x_start, x_end):
-            self.board[x][y_bottom_left].setObstacle(x, y_bottom_left)
+            self.board[x][y_bottom_left].setObstacle()
 
         x_start = x_end
-        x_end = 84 + 576 + 942 + 93
+        x_end = 42 + 288 + 471 + 46
         for x in range(x_start, x_end):
-            self.board[x][y_bottom_left].setObstacle(x, y_bottom_left)
-            if(x % 9 == 0):
+            self.board[x][y_bottom_left].setObstacle()
+            if(x % 5 == 0):
                 y_bottom_left -= 1
 
     def drawInnerWalls(self):
         y_top = self.boardHeight - 66
         y_bottom = self.boardHeight - 16
         x_start = 0
-        x_end = 283
+        x_end = 141
         for x in range(x_end):
-            self.board[x][y_top].setObstacle(x, y_top)
-            self.board[x][y_bottom].setObstacle(x, y_bottom)
-            if(x < 84 and x % 14 == 0):
+            self.board[x][y_top].setObstacle()
+            self.board[x][y_bottom].setObstacle()
+            if(x < 42 and x % 7 == 0):
                 y_bottom += 1
-            if(x >= 84 and x % 57 == 0):
+            if(x >= 42 and x % 28 == 0):
                 y_bottom -= 1
-            if(x % 57 == 0):
+            if(x % 28 == 0):
                 y_top -= 1
 
-        x_start = x_end + 6
-        x_end = x_start + 375
+        x_start = x_end + 3
+        x_end = x_start + 188
 
         for x in range(x_start, x_end):
-            self.board[x][y_top].setObstacle(x, y_top)
-            self.board[x][y_bottom].setObstacle(x, y_bottom)
-            if( x % 57 == 0):
+            self.board[x][y_top].setObstacle()
+            self.board[x][y_bottom].setObstacle()
+            if( x % 28 == 0):
                 y_top -= 1
                 y_bottom -= 1
 
         for i in range(2):
-            x_start = x_end + 6
-            x_end = x_start + 375
+            x_start = x_end + 3
+            x_end = x_start + 188
 
             for x in range(x_start, x_end):
-                self.board[x][y_top].setObstacle(x, y_top)
-                self.board[x][y_bottom].setObstacle(x, y_bottom)
+                self.board[x][y_top].setObstacle()
+                self.board[x][y_bottom].setObstacle()
